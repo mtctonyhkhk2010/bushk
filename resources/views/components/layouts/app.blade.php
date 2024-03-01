1a9408f72d997e9fcc2487bbd3d94840e7b4c722
brucemak <mtctonyhkhk2010@gmail.com>
2024-03-01 17:35:15 +0800

    ask for geolocation directly, no more bullshit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -26,22 +26,11 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <script>
-        navigator.permissions.query({name:'geolocation'})
-            .then((result) => {
-                    if (result.state === 'granted') {
-                        window.watch_position_id = navigator.geolocation.watchPosition((position) => {
-                            window.coords = position.coords;
-                            const event = new CustomEvent("position-updated");
-                            document.dispatchEvent(event);
-                        });
-                    } else {
-                        console.log('Browser location services disabled', navigator);
-                        navigator.geolocation.getCurrentPosition(() => {})
-                    }
-                }, () => {
-                    console.log('Browser permissions services unavailable', navigator);
-                }
-            );
+        window.watch_position_id = navigator.geolocation.watchPosition((position) => {
+            window.coords = position.coords;
+            const event = new CustomEvent("position-updated");
+            document.dispatchEvent(event);
+        });
     </script>
 </head>
 <body class="font-sans antialiased bg-white dark:bg-base-200">
