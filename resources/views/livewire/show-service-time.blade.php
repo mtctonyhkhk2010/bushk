1a4ef1c1f7dab95cab727d71c188838f6a0e41a3
brucemak <mtctonyhkhk2010@gmail.com>
2024-03-14 15:45:16 +0800

    update service time display

--- resources/views/livewire/show-service-time.blade.php
+++ resources/views/livewire/show-service-time.blade.php
@@ -20,7 +20,7 @@
             @foreach($service_times as $service_time)
                 <div class="font-bold mt-3">{{ $service_time[0]['weekday_tc'] }}</div>
                 @foreach($service_time as $time_period)
-                    <div>{{ $time_period['start'] }} - {{ $time_period['end'] }} {{ $time_period['frequency_min'] }} 分鐘</div>
+                    <div>{{ $time_period['start'] }}@isset($time_period['end']) - {{ $time_period['end'] }}@endif@isset($time_period['frequency_min']) {{ $time_period['frequency_min'] }} 分鐘@endif</div>
                 @endforeach
             @endforeach
         @endif
