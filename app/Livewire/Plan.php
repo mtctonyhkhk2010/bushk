b58824838b159a7db852c175cd86b07471436890
brucemak <mtctonyhkhk2010@gmail.com>
2024-04-03 16:02:08 +0800

    plan wip

--- app/Livewire/Plan.php
+++ app/Livewire/Plan.php
@@ -2,11 +2,28 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use function Laravel\Prompts\search;
 
 class Plan extends Component
 {
+    public $from;
+
+    public $to;
+
+    public $show_from_suggestion = false;
+
+    public $show_to_suggestion = false;
+    public $from_location;
+
+    public $to_location;
+
+    public $from_suggestion = [];
+    public $to_suggestion = [];
+
     public function mount()
     {
 
@@ -16,30 +33,60 @@ class Plan extends Component
         return view('livewire.plan');
     }
 
+    public function saveFromPosition($key)
+    {
+        $this->from_location = $this->from_suggestion['places'][$key];
+        $this->from = $this->from_suggestion['places'][$key]['displayName']['text'];
+        $this->show_from_suggestion = false;
+
+    }
+
+    public function saveToPosition($key)
+    {
+        $this->to_location = $this->to_suggestion['places'][$key];
+        $this->to = $this->to_suggestion['places'][$key]['displayName']['text'];
+        $this->show_to_suggestion = false;
+    }
+
+    public function updatedFrom()
+    {
+        $this->show_from_suggestion = true;
+        $this->from_location = [];
+        $this->from_suggestion = $this->searchLocation($this->from);
+    }
+
+    public function updatedTo()
+    {
+        $this->show_to_suggestion = true;
+        $this->to_location = [];
+        $this->to_suggestion = $this->searchLocation($this->to);
+    }
+
     public function searchLocation($name)
     {
         if (empty($name)) return [];
 
-        $result = Http::withHeaders([
-            'X-Goog-Api-Key' => config('google.API_KEY'),
-            'X-Goog-FieldMask' => 'places.displayName,places.formattedAddress,places.location'
-        ])->post('https://places.googleapis.com/v1/places:searchText', [
-            'textQuery' => $name,
-            'languageCode' => 'zh-HK',
-            'locationRestriction' => [
-                'rectangle' => [
-                    'high' => [
-                        'latitude' => '22.560497309421372',
-                        'longitude' => '114.41298578594423',
-                    ],
-                    'low' => [
-                        'latitude' => '22.1947741673399',
-                        'longitude' => '113.83689021624464',
-                    ],
+        //cache google result for a day
+        return Cache::remember('1searchLocation_' . $name, 86400, function () use ($name) {
+            return Http::withHeaders([
+                'X-Goog-Api-Key'   => config('google.API_KEY'),
+                'X-Goog-FieldMask' => 'places.displayName,places.formattedAddress,places.location'
+            ])->post('https://places.googleapis.com/v1/places:searchText', [
+                'textQuery'           => $name,
+                'languageCode'        => 'zh-HK',
+                'locationRestriction' => [
+                    'rectangle' => [
+                        'high' => [
+                            'latitude'  => '22.560497309421372',
+                            'longitude' => '114.41298578594423',
+                        ],
+                        'low'  => [
+                            'latitude'  => '22.1947741673399',
+                            'longitude' => '113.83689021624464',
+                        ],
+                    ]
                 ]
-            ]
-        ]);
-
-        return $result->json();
+            ])->json();
+        });
     }
 }
--- resources/views/livewire/plan.blade.php
+++ resources/views/livewire/plan.blade.php
@@ -2,18 +2,46 @@
     <x-layouts.navbar :title="'Plan'">
 
     </x-layouts.navbar>
-    <div class="h-[calc(100svh-112px-env(safe-area-inset-bottom))]">
+    <div class="h-[calc(100svh-112px-env(safe-area-inset-bottom))] px-3">
         <label class="form-control w-full max-w-xs">
             <div class="label">
                 <span class="label-text">From</span>
             </div>
-            <input type="text" placeholder="Your location" class="input input-bordered w-full max-w-xs" wire:model="from"/>
+            <div class="dropdown">
+                <input type="text"
+                       placeholder="Your location"
+                       class="input input-bordered w-full max-w-xs
+                       @if(!empty($from) && empty($this->from_location)) input-warning @endif
+                       @if(!empty($from) && !empty($this->from_location)) input-success @endif
+                       "
+                       wire:model.live="from"/>
+                @if(!empty($this->from_suggestion) && $show_from_suggestion)
+                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
+                        @foreach($this->from_suggestion['places'] as $key => $suggestion)
+                            <li><a wire:click="saveFromPosition({{ $key }})">{{ $suggestion['displayName']['text'] }}</a></li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div>
         </label>
         <label class="form-control w-full max-w-xs">
             <div class="label">
                 <span class="label-text">To</span>
             </div>
-            <input type="text" placeholder="To" class="input input-bordered w-full max-w-xs" wire:model="to"/>
+            <input type="text"
+                   placeholder="To"
+                   class="input input-bordered w-full max-w-xs
+                   @if(!empty($to) && empty($this->to_location)) input-warning @endif
+                   @if(!empty($to) && !empty($this->to_location)) input-success @endif
+                   "
+                   wire:model.live="to"/>
+            @if(!empty($this->to_suggestion) && $show_to_suggestion)
+                <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    @foreach($this->to_suggestion['places'] as $key => $suggestion)
+                        <li><a wire:click="saveToPosition({{ $key }})">{{ $suggestion['displayName']['text'] }}</a></li>
+                    @endforeach
+                </ul>
+            @endif
         </label>
     </div>
     <x-offline/>
